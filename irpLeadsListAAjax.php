63c84f5baa8cc70a7c470c7eb8ce0ba0dfc07096
ap-clf <164407396+ap-clf@users.noreply.github.com>
2024-03-21 21:03:28 +0100

    MagMark

--- irpLeadsListAAjax.php
+++ irpLeadsListAAjax.php
@@ -56,13 +56,13 @@ SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (58) AND ss
 UNION 
 SELECT  ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (9,46,48,49,51,52)
 UNION 
-SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (15,16,18,21,23,38,54,55, 40,50,53,65) AND ss.status IN ('otwarta')
+SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (15,16,18,21,23,38,54,55,40,50,53,65,62) AND ss.status IN ('otwarta')
 UNION 
 SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (40,50,53,65) AND ss.status IN ('wygrana')
 UNION 
 SELECT t.klient_id FROM `tagi` t WHERE t.tagId IN (10,11,24,32)
 UNION 
-SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (40,50,53,65, 15,16,18,21,23,38,54,55) 
+SELECT ss.klient_id FROM `szanseSprzedazy` ss WHERE ss.id_procesu IN (40,50,53,65, 15,16,18,21,23,38,54,55,62) 
     AND ss.status IN('przegrana') AND ss.data_zakonczenia > date_sub(curdate(),interval 1 MONTH)	
 UNION 
 SELECT z.klient_id FROM zamowienia z JOIN faktury f ON f.id_zamowienia = z.id WHERE z.status IN('do opłacenia') AND z.zaplacone = 'nie' AND f.rodzaj_faktury = 'VAT'
@@ -82,24 +82,8 @@ CREATE TEMPORARY TABLE __wykluczenia (klient_id INT NOT NULL, PRIMARY KEY (klien
 AS SELECT klient_id FROM __wykluczenia0
 UNION SELECT klient_id FROM __wykluczenia1
 UNION SELECT klient_id FROM __wykluczenia2;
-
-
-SELECT k.id, k.imie, k.nazwisko, k.email, k.telefon, k.level
-FROM `klienci` k 
-JOIN uczestnicyProgramu u ON u.klient_id = k.id
-JOIN (
-    SELECT uczestnik_id, MAX(produkt_id) as max 
-    FROM `uczestnicyProgramuProdukty` t1 
-    WHERE t1.status = 'wyslane' 
-    GROUP BY uczestnik_id 
-    HAVING max=118
-) AS t2 ON t2.uczestnik_id = u.uczestnik_id
-WHERE
-    k.level = 'uczestnik' 
-    AND k.status = 'aktywny'
-    AND NOT EXISTS(SELECT 1 FROM __wykluczenia w WHERE w.klient_id = k.id);
     
-SELECT k.id, k.imie, k.nazwisko, k.email, k.telefon, k.level
+SELECT k.id, k.imie, k.nazwisko, k.email, k.telefon, k.level, p.nazwa
 FROM `klienci` k 
 JOIN uczestnicyProgramu u ON u.klient_id = k.id
 JOIN (
@@ -107,8 +91,9 @@ JOIN (
     FROM `uczestnicyProgramuProdukty` t1 
     WHERE t1.status = 'wyslane' 
     GROUP BY uczestnik_id 
-    HAVING max>118
+    HAVING max>=118
 ) AS t2 ON t2.uczestnik_id = u.uczestnik_id
+LEFT JOIN `produkty` p ON t2.max = p.id
 WHERE
     k.level = 'uczestnik' 
     AND k.status = 'aktywny'
@@ -127,11 +112,12 @@ WHERE
             $data[$i] = [];
             while ($w = mysqli_fetch_assoc($result)) {
                 $data[$i][] = array(
-                    'klient_id' => $w['id'],
-                    'klient'    => $w['imie'] . ' ' . $w['nazwisko'],
-                    'telefon'   => $w['telefon'],
-                    'email'     => $w['email'],
-                    'level'     => $w['level']
+                    'klient_id'     => $w['id'],
+                    'klient'        => $w['imie'] . ' ' . $w['nazwisko'],
+                    'telefon'       => $w['telefon'],
+                    'email'         => $w['email'],
+                    'level'         => $w['level'],
+		    'produkt_nazwa' => $w['level']
                 );
             }
             $result->free();
